81e8aa800c6485b0fe46dd3283253eaaf93aad0b
Justin <janton3017@stu.d214.org>
2020-11-09 22:44:10 -0800

    Initial Commit

---
+++ ViewLecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewLecture
+//
+//  Created by user172402 on 11/9/20.
+//  Copyright © 2020 John Hersey Highschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
